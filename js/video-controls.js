9ca855b4f9793642ad21da672ef785da47e4c957
James Kao <james@thekao.com>
2015-04-10 22:06:56 -0700

    rm bad comment style

--- js/video-controls.js
+++ js/video-controls.js
@@ -1,9 +1,7 @@
 var v = document.getElementById('promo_video');
 
-<!-- Preset volume -->
 v.volume = 0.5;
 
-<!-- Play, Pause -->
 v.addEventListener('click',function(){
     if (v.paused == false) {
         v.pause();
